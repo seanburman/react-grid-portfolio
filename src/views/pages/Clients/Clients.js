e774eddaa287155ea2ea97b5cbfbf603bbb1dd92
Sean Burman <sean.burman@icloud.com>
2021-05-12 12:44:14 -0400

    Added Nathalie Anne to Clients

--- src/views/pages/Clients/Clients.js
+++ src/views/pages/Clients/Clients.js
@@ -5,6 +5,25 @@ export default function Clients() {
         <div className="grid-container-col-1 fade-in">
             <div className="grid-container-client">
                 <div className="client-description-container">
+                <p className="client-description">Nathalie Anne is a multi-disciplinary creative with a passion for graphic design, illustration, and branding.</p>
+                </div>
+                <div className="logo-container">
+                    <img 
+                        src="/img/NathalieLogo.png" 
+                        alt="Nathalie Anne Design" 
+                        className="client-logo"/>
+                </div>
+                <div className="client-button-container">
+                <a className="client-link" href="https://nathalieannedesign.com" target="blank">
+                    <button className="client-button">See More</button>
+                </a>
+                </div>
+            </div>
+
+        <div className="hz-rule"></div>
+
+            <div className="grid-container-client">
+                <div className="client-description-container">
                 <p className="client-description">Catherine Clark Connects is a boutique mental health consulting firm that specializes in helping individuals and organizations proactively address mental health, create a safe space for vulnerable conversations, and build more meaningful, fulfilled lives.</p>
                 </div>
                 <div className="logo-container">
@@ -18,7 +37,7 @@ export default function Clients() {
                     <button className="client-button">See More</button>
                 </a>
                 </div>
-        </div>
+            </div>
         
             
         <div className="hz-rule"></div>
